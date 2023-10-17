bf64ef7962dd3c80f1cc244661f81adc704f3257
Abdullah Alqahtani <0xqahta0@gmail.com>
2023-10-18 00:54:52 +0300

    feat: connect to db & configure alembic

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,8 +1,13 @@
 from api.controllers.market_controllers import router as MarketRouter
 from api.controllers.rules_controller import router as RuleController
+from api.controllers.alerts_controller import router as AlertController
 
 
 def init_routes(app):
-    app.include_router(MarketRouter, prefix="/market-prices", tags=["Market"])
-    app.include_router(RuleController, prefix="/alert-rules", tags=["Rules"])
+    app.include_router(
+        MarketRouter, prefix="/api/v1/market-prices", tags=["Market"])
+    app.include_router(
+        RuleController, prefix="/api/v1/alert-rules", tags=["Rules"])
+    app.include_router(
+        AlertController, prefix="/api/v1/alerts", tags=["Alerts"])
     return app
--- src/db/session.py
+++ src/db/session.py
@@ -1,10 +1,12 @@
 import os
+import time
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from db.models.model_base import Base
+from utils import utils
 
-DATABASE_URL = os.environ.get(
-    "DATABASE_URL", "cockroachdb://root@localhost:26257/investor_bulletin?sslmode=disable")
-
+utils.load_environment_variables()
+DATABASE_URL = os.environ.get("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
